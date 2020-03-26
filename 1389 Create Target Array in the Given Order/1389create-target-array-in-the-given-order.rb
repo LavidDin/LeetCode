e4b83dde9fa4cd06eb26d5333d46af8bd3e1cbba
David Lin <polsara@mac.home>
2020-03-26 04:38:51 +0000

    Code challenge submission for question: 1389 Create Target Array in the Given Order

---
+++ 1389 Create Target Array in the Given Order/1389create-target-array-in-the-given-order.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @param {Integer[]} index
+# @return {Integer[]}
+def create_target_array(nums, index)
+    target_array = []
+    
+    nums.each.with_index do |num, idx|
+        index[idx]
+        target_array.insert(index[idx], num)
+    end
+    target_array
+end
