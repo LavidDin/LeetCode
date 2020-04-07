6d884d338642fa6daedb0312d676c9cbf356949c
David Lin <polsara@mac.home>
2020-04-07 03:29:27 +0000

    Code challenge submission for question: 202 Happy Number

---
+++ 202 Happy Number/202happy-number.rb
@@ -0,0 +1,12 @@
+def is_happy(n)
+  current = n
+  previous_vals = [n]
+  
+  while current != 1
+    current = current.to_s.split('').reduce(0) { |sum, num| sum + num.to_i**2 }
+    return false if previous_vals.include?(current)
+    previous_vals << current
+  end
+  
+  true
+end
