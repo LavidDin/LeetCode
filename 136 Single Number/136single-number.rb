5e0d805a3d14632037ef738cf982278937968732
David Lin <polsara@mac.home>
2020-04-07 02:06:27 +0000

    Code challenge submission for question: 136 Single Number

---
+++ 136 Single Number/136single-number.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def single_number(nums)
+    counts = Hash.new(0)
+    nums.each { |num| counts[num] += 1 }
+    counts.each do |k, v|
+        return key = k if counts[k] == 1
+    end
+end
