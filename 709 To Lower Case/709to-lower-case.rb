335daf352b5423724eaed76277844a73dde1a4ec
David Lin <polsara@mac.home>
2020-08-31 15:27:13 +0000

    Code challenge submission for question: 709 To Lower Case

---
+++ 709 To Lower Case/709to-lower-case.rb
@@ -0,0 +1,15 @@
+# @param {String} str
+# @return {String}
+def to_lower_case(str)
+  arr = []
+  str.each_char do |char|
+    if char.ord < 97 && char.ord > 64
+      lower = char.ord + 32
+      arr << lower.chr
+    else
+      arr << char
+    end
+  end
+
+   arr.join('')  
+end
