51c98096af263666ad160d9a55653558963e1ba3
David Lin <polsara@mac.home>
2020-08-31 19:33:49 +0000

    Code challenge submission for question: 183 Customers Who Never Order

---
+++ 183 Customers Who Never Order/183customers-who-never-order.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    Customers.Name AS Customers
+FROM
+    Customers
+LEFT JOIN
+    Orders ON Orders.CustomerId = Customers.Id
+WHERE
+    Orders.Id IS NULL
