44407820e2fdf9c1487c90847d477016e88861b7
David Lin <polsara@mac.home>
2020-09-06 20:10:09 +0000

    Code challenge submission for question: 121 Best Time to Buy and Sell Stock

---
+++ 121 Best Time to Buy and Sell Stock/121best-time-to-buy-and-sell-stock.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    return 0 if prices.length < 2
+    
+    max_profit = 0
+    min_price = prices[0]
+    
+    (1..prices.length - 1).each do |i|
+        min_price = [min_price, prices[i-1]].min
+        max_profit = [max_profit, prices[i] - min_price].max
+    end
+    
+    max_profit
+end
