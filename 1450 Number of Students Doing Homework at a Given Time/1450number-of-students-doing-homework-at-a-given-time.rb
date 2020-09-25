afd42b449d266b01102c7170ea1fffd7c1eb7737
David Lin <polsara@mac.home>
2020-09-25 10:29:36 +0000

    Code challenge submission for question: 1450 Number of Students Doing Homework at a Given Time

---
+++ 1450 Number of Students Doing Homework at a Given Time/1450number-of-students-doing-homework-at-a-given-time.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} start_time
+# @param {Integer[]} end_time
+# @param {Integer} query_time
+# @return {Integer}
+def busy_student(start_time, end_time, query_time)
+  count = 0
+  (0...start_time.length).each do |i|
+    count += 1 if (start_time[i] <= query_time) && (query_time <= end_time[i])
+  end
+
+  count
+end
