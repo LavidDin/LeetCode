0170e04bc4c8932c3e255ba830648f56ee534b8b
David Lin <polsara@mac.home>
2020-09-30 11:29:51 +0000

    Code challenge submission for question: 852 Peak Index in a Mountain Array

---
+++ 852 Peak Index in a Mountain Array/852peak-index-in-a-mountain-array.rb
@@ -0,0 +1,7 @@
+# @param {Integer[]} arr
+# @return {Integer}
+def peak_index_in_mountain_array(arr)
+  (1...arr.length-1).each do |i|
+    return i if (arr[i-1] < arr[i]) && (arr[i] > arr[i+1])
+  end
+end
