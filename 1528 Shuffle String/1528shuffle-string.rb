23a877aec002dfd835018813f110d2a017f27f9a
David Lin <polsara@mac.home>
2020-10-06 11:01:34 +0000

    Code challenge submission for question: 1528 Shuffle String

---
+++ 1528 Shuffle String/1528shuffle-string.rb
@@ -0,0 +1,11 @@
+# @param {String} s
+# @param {Integer[]} indices
+# @return {String}
+def restore_string(s, indices)
+  restored = Array.new(s.length , ' ')
+
+  indices.each_with_index do |char_idx, i|
+    restored[char_idx] = s[i]
+  end
+  restored.join('')
+end
