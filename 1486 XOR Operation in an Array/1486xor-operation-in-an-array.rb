426076d57862f3a2de54900bf8b44c5c3d8792ee
David Lin <polsara@mac.home>
2020-10-07 19:21:46 +0000

    Code challenge submission for question: 1486 XOR Operation in an Array

---
+++ 1486 XOR Operation in an Array/1486xor-operation-in-an-array.rb
@@ -0,0 +1,10 @@
+# @param {Integer} n
+# @param {Integer} start
+# @return {Integer}
+def xor_operation(n, start)
+    result = start
+    for i in 1...n
+        result = result ^ (start + 2*i)
+    end
+    return result
+end
