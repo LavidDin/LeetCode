9e22a52676a450beb88a38eb4ae1e4e470505c4d
David Lin <polsara@mac.home>
2020-10-10 13:09:08 +0000

    Code challenge submission for question: 1266 Minimum Time Visiting All Points

---
+++ 1266 Minimum Time Visiting All Points/1266minimum-time-visiting-all-points.rb
@@ -0,0 +1,11 @@
+# @param {Integer[][]} points
+# @return {Integer}
+def min_time_to_visit_all_points(points)
+    seconds = 0
+    (0...points.length-1).each do |i|
+      x = (points[i+1][0] - points[i][0]).abs
+      y = (points[i+1][1] - points[i][1]).abs
+      x > y ? seconds += x : seconds += y
+    end
+    return seconds
+end
