f06eca889340fb937d5287813ad644a76ee19e09
David Lin <polsara@mac.home>
2020-10-12 15:22:23 +0000

    Code challenge submission for question: 1614 Maximum Nesting Depth of the Parentheses

---
+++ 1614 Maximum Nesting Depth of the Parentheses/1614maximum-nesting-depth-of-the-parentheses.rb
@@ -0,0 +1,14 @@
+# @param {String} s
+# @return {Integer}
+def max_depth(s)
+    depth, open = 0, 0
+    s.each_char do |c|
+        if c == '('
+          open += 1
+        elsif c == ')'
+          open -= 1
+        end
+      depth = [depth, open].max
+    end
+  return depth
+end
