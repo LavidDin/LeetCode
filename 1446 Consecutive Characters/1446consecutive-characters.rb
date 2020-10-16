d8a49dc15428d7c7c72d4743bbf60e0f3d5a8977
David Lin <polsara@mac.home>
2020-10-16 20:05:01 +0000

    Code challenge submission for question: 1446 Consecutive Characters

---
+++ 1446 Consecutive Characters/1446consecutive-characters.rb
@@ -0,0 +1,18 @@
+# @param {String} s
+# @return {Integer}
+def max_power(s)
+  power = 1
+  s_arr = s.split('')
+
+  i = 0
+  while i < s_arr.length-1
+    j = 1
+    while s_arr[i] == s_arr[i+j]
+      j += 1
+      power = j if j > power
+    end
+    i += j
+  end
+
+  power
+end
