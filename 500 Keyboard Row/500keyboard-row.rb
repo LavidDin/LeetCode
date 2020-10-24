4ae9f440451e20416c25c1a521722f6b54fb5a10
David Lin <polsara@mac.home>
2020-10-24 14:48:34 +0000

    Code challenge submission for question: 500 Keyboard Row

---
+++ 500 Keyboard Row/500keyboard-row.rb
@@ -0,0 +1,9 @@
+# @param {String[]} words
+# @return {String[]}
+def find_words(words)
+  r1 = 'qwertyuiop'
+  r2 = 'asdfghjkl'
+  r3 = 'zxcvbnm'
+
+  words.select{|word| word.chars.all?{|c| r1.include?(c.downcase)} || word.chars.all?{|c| r2.include?(c.downcase)} || word.chars.all?{|c| r3.include?(c.downcase)}}
+end
