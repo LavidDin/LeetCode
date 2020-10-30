3b80d6bcfba1de92a202cca315340f21a98c49d3
David Lin <polsara@mac.home>
2020-10-30 14:16:44 +0000

    Code challenge submission for question: 1475 Final Prices With a Special Discount in a Shop

---
+++ 1475 Final Prices With a Special Discount in a Shop/1475final-prices-with-a-special-discount-in-a-shop.rb
@@ -0,0 +1,19 @@
+# @param {Integer[]} prices
+# @return {Integer[]}
+def final_prices(prices)
+  final_prices = []
+
+  prices.each_with_index do |e, i|
+    j = i+1
+    while j < prices.length
+      if prices[j] <= e
+        e -= prices[j]
+        break
+      end
+      j += 1
+    end
+    final_prices << e
+  end
+
+  final_prices
+end
