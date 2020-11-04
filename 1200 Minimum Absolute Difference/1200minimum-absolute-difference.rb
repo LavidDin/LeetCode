5dd29d6da7a43d8ae5355caa8fe282892e9a2494
David Lin <polsara@mac.home>
2020-11-04 15:04:05 +0000

    Code challenge submission for question: 1200 Minimum Absolute Difference

---
+++ 1200 Minimum Absolute Difference/1200minimum-absolute-difference.rb
@@ -0,0 +1,20 @@
+# @param {Integer[]} arr
+# @return {Integer[][]}
+def minimum_abs_difference(arr)
+  arr.sort!
+  min_diff = Float::INFINITY
+  diff_arr = []
+
+  (0...arr.length-1).each do |i|
+    diff = (arr[i+1] - arr[i]).abs
+
+    if diff < min_diff
+      min_diff = diff
+      diff_arr = [[arr[i], arr[i+1]]]
+    elsif diff == min_diff
+      diff_arr << [arr[i], arr[i+1]]
+    end
+  end
+
+  diff_arr
+end
