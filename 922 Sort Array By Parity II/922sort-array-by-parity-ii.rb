5f7a6ff5dbfff7f625943386813dbb0b5937402f
David Lin <polsara@mac.home>
2020-11-05 15:09:01 +0000

    Code challenge submission for question: 922 Sort Array By Parity II

---
+++ 922 Sort Array By Parity II/922sort-array-by-parity-ii.rb
@@ -0,0 +1,18 @@
+# @param {Integer[]} a
+# @return {Integer[]}
+def sort_array_by_parity_ii(a)
+  sorted = []
+  e,o = 0,1
+
+  a.each do |i|
+    if i.even?
+      sorted[e] = i
+      e += 2
+    else
+      sorted[o] = i
+      o += 2
+    end
+  end
+
+  sorted
+end
