a66df84b93ef13750e79441f818d7234166eed21
David Lin <polsara@mac.home>
2020-11-05 21:18:03 +0000

    Code challenge submission for question: 1185 Day of the Week

---
+++ 1185 Day of the Week/1185day-of-the-week.rb
@@ -0,0 +1,9 @@
+# @param {Integer} day
+# @param {Integer} month
+# @param {Integer} year
+# @return {String}
+require 'date'
+
+def day_of_the_week(day, month, year)
+  Date.new(year,month,day).strftime('%A')
+end
