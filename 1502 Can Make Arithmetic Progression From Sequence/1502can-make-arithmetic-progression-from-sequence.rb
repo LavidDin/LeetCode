d4f227286c2f06e7b3dbb3150485a3d041766bd9
David Lin <polsara@mac.home>
2020-11-09 14:56:33 +0000

    Code challenge submission for question: 1502 Can Make Arithmetic Progression From Sequence

---
+++ 1502 Can Make Arithmetic Progression From Sequence/1502can-make-arithmetic-progression-from-sequence.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} arr
+# @return {Boolean}
+def can_make_arithmetic_progression(arr)
+  arr.sort!
+  diff = arr[1] - arr[0]
+  (0...arr.length-1).each { |i| return false if arr[i+1] - arr[i] != diff }
+  true
+end
