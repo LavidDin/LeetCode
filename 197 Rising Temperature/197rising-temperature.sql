8c0337b1618b84836b2e0735ba02f5e029c3d3d3
David Lin <polsara@mac.home>
2020-11-21 13:32:44 +0000

    Code challenge submission for question: 197 Rising Temperature

---
+++ 197 Rising Temperature/197rising-temperature.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT Id from Weather w1
+WHERE w1.Temperature > (
+    SELECT w2.Temperature from Weather w2
+    WHERE RecordDate = subdate(w1.RecordDate, 1)
+);
