1b5fc3a2d348202714e2b9771e98971cc5735964
David Lin <polsara@mac.home>
2020-11-23 14:06:27 +0000

    Code challenge submission for question: 66 Plus One

---
+++ 66 Plus One/66plus-one.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} digits
+# @return {Integer[]}
+def plus_one(digits)
+  arr = (digits.join('').to_i + 1).to_s.split('').map(&:to_i)
+    
+  arr.unshift(0) if digits[0] == 0 && digits.length > 1
+
+  arr
+end
