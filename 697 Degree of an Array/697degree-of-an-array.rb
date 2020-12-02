00935790781feccee2b02934be2a8c9c35c4f280
David Lin <polsara@mac.home>
2020-12-02 12:51:18 +0000

    Code challenge submission for question: 697 Degree of an Array

---
+++ 697 Degree of an Array/697degree-of-an-array.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def find_shortest_sub_array(nums)
+  h = Hash.new(0)
+  nums.each { |n| h[n] += 1}
+  max = h.values.max
+  #return 1 if max == 1
+  max_hash = Hash[h.select { |k, v| v == max}]
+
+  max_hash.keys.map { |k| nums.rindex(k) - nums.index(k) + 1 }.min
+end
