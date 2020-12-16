7d072fec8962392b34bf85d4deedf83cc14626de
David Lin <polsara@mac.home>
2020-12-16 13:33:20 +0000

    Code challenge submission for question: 1385 Find the Distance Value Between Two Arrays

---
+++ 1385 Find the Distance Value Between Two Arrays/1385find-the-distance-value-between-two-arrays.rb
@@ -0,0 +1,7 @@
+# @param {Integer[]} arr1
+# @param {Integer[]} arr2
+# @param {Integer} d
+# @return {Integer}
+def find_the_distance_value(arr1, arr2, d)
+  arr1.count { |i| arr2.all? { |j| (i-j).abs > d } }
+end
