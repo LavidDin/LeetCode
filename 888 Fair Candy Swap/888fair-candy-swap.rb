9a50dd8523409b536c3849d4662bd34897cc5046
David Lin <polsara@mac.home>
2020-12-23 17:30:39 +0000

    Code challenge submission for question: 888 Fair Candy Swap

---
+++ 888 Fair Candy Swap/888fair-candy-swap.rb
@@ -0,0 +1,8 @@
+# @param {Integer[]} a
+# @param {Integer[]} b
+# @return {Integer[]}
+def fair_candy_swap(a, b)
+    dif = (a.sum - b.sum) / 2
+    hash = a.each_with_object({}) { |x, obj| obj[x - dif] = x }
+    b.each { |x| return [hash[x], x] unless hash[x].nil? }
+end
