3e7a86ed5b73392fb769e7273ac910f5f59047e4
David Lin <polsara@mac.home>
2020-12-27 14:51:22 +0000

    Code challenge submission for question: 1160 Find Words That Can Be Formed by Characters

---
+++ 1160 Find Words That Can Be Formed by Characters/1160find-words-that-can-be-formed-by-characters.rb
@@ -0,0 +1,15 @@
+# @param {String[]} words
+# @param {String} chars
+# @return {Integer}
+def count_characters(words, chars)
+  count = 0
+
+  words.each do |word|
+    word = word.split('')
+    count += word.length if word.none? do |char|
+      word.count(char) > chars.count(char)
+    end
+  end
+
+  count
+end
