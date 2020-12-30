2df1f29b0141036f2933a9f1ea7aece75e6818bf
David Lin <polsara@mac.home>
2020-12-30 13:55:52 +0000

    Code challenge submission for question: 908 Smallest Range I

---
+++ 908 Smallest Range I/908smallest-range-i.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} a
+# @param {Integer} k
+# @return {Integer}
+def smallest_range_i(a, k)
+  min = a.min
+  max = a.max
+  diff = max - min
+
+  2 * k >= diff ? 0 : diff - 2 * k
+end
