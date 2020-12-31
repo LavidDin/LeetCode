92a59acdd8b10fdc57d1f9f28becef51682a4ce1
David Lin <polsara@mac.home>
2020-12-31 16:26:20 +0000

    Code challenge submission for question: 893 Groups of Special-Equivalent Strings

---
+++ 893 Groups of Special-Equivalent Strings/893groups-of-special-equivalent-strings.rb
@@ -0,0 +1,15 @@
+# @param {String[]} a
+# @return {Integer}
+def num_special_equiv_groups(a)
+  sorted = []
+  a.each do |str|
+    odd_chars = []
+    even_chars = []
+    str.each_char.with_index do |char, i|
+      i.even? ? even_chars << char : odd_chars << char
+    end
+    sorted << (odd_chars.sort + even_chars.sort).join('')
+  end
+
+  sorted.uniq.length
+end
