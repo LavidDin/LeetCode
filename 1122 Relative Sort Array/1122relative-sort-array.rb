9962756caa5dc7b65bdb30e019d79fded2332c64
David Lin <polsara@mac.home>
2021-01-01 18:37:10 +0000

    Code challenge submission for question: 1122 Relative Sort Array

---
+++ 1122 Relative Sort Array/1122relative-sort-array.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} arr1
+# @param {Integer[]} arr2
+# @return {Integer[]}
+def relative_sort_array(arr1, arr2)
+  sorted = []
+
+  arr2.each do |i|
+    arr1.count(i).times { sorted << i }
+    arr1.delete(i)
+  end
+
+  sorted.push(arr1.sort!).flatten!
+end
