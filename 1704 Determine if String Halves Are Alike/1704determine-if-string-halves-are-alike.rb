b71a9e747cd97e3e5e81f5346929f86201fc6c4d
David Lin <polsara@mac.home>
2021-01-04 17:09:07 +0000

    Code challenge submission for question: 1704 Determine if String Halves Are Alike

---
+++ 1704 Determine if String Halves Are Alike/1704determine-if-string-halves-are-alike.rb
@@ -0,0 +1,9 @@
+# @param {String} s
+# @return {Boolean}
+def halves_are_alike(s)
+  vowels = ['a','e','i','o','u']
+  first = s.downcase[0...(s.length / 2)]
+  second = s.downcase[(s.length / 2)..-1]
+
+  first.chars.count { |char| vowels.include?(char) } == second.chars.count { |char| vowels.include?(char )}
+end
