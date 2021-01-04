1ed4f2fecf686f2bce940d2413ae63104b51a1e0
David Lin <polsara@mac.home>
2021-01-04 15:13:04 +0000

    Code challenge submission for question: 1700 Number of Students Unable to Eat Lunch

---
+++ 1700 Number of Students Unable to Eat Lunch/1700number-of-students-unable-to-eat-lunch.rb
@@ -0,0 +1,15 @@
+# @param {Integer[]} students
+# @param {Integer[]} sandwiches
+# @return {Integer}
+def count_students(students, sandwiches)
+  until students.all? { |student| student != sandwiches.first } || students.empty?
+    if students.first == sandwiches.first
+      sandwiches.shift
+      students.shift
+    else
+      students << students.shift
+    end
+  end
+
+  sandwiches.count
+end
