c006b685d840545dcb976c03cfcf7f435eeac102
David Lin <polsara@mac.home>
2021-02-01 18:05:13 +0000

    Code challenge submission for question: 724 Find Pivot Index

---
+++ 724 Find Pivot Index/724find-pivot-index.rb
@@ -0,0 +1,14 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def pivot_index(nums)
+  right_sum = nums.sum
+  left_sum = 0
+
+  (0...nums.length).each do |i|
+    right_sum -= nums[i]
+    return i if left_sum == right_sum
+    left_sum += nums[i]
+  end
+
+  return -1
+end
