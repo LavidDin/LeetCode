d6e8604aa2d56c511bc4a97adfa43e4864bc2bf2
David Lin <polsara@mac.home>
2021-02-11 18:31:27 +0000

    Code challenge submission for question: 541 Reverse String II

---
+++ 541 Reverse String II/541reverse-string-ii.rb
@@ -0,0 +1,9 @@
+# @param {String} s
+# @param {Integer} k
+# @return {String}
+def reverse_str(s, k)
+  arr = s.chars.each_slice(k).map{|c| c.join}
+  (0...arr.size).step(2) { |i| arr[i] = arr[i].reverse }
+
+  arr.join('')
+end
