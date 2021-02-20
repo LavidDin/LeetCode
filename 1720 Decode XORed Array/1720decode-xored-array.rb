34d3e3ffd48068d3a2f113ca53421adc6ddbbc25
David Lin <polsara@mac.home>
2021-02-20 18:50:31 +0000

    Code challenge submission for question: 1720 Decode XORed Array

---
+++ 1720 Decode XORed Array/1720decode-xored-array.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} encoded
+# @param {Integer} first
+# @return {Integer[]}
+def decode(encoded, first)
+  arr = [first]
+  
+  encoded.each do |i|
+    xor =  arr[-1]^i
+    arr << xor
+  end
+
+  arr
+end
