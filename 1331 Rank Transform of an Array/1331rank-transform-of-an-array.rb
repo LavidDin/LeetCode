8237f8e1b91dee83e5e6e1b4907a896fe1077b55
David Lin <polsara@mac.home>
2021-02-28 21:24:18 +0000

    Code challenge submission for question: 1331 Rank Transform of an Array

---
+++ 1331 Rank Transform of an Array/1331rank-transform-of-an-array.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} arr
+# @return {Integer[]}
+def array_rank_transform(arr)
+  sorted = arr.uniq.sort
+  rank = {}
+  (0...sorted.length).each { |i| rank[sorted[i]] = i + 1 }
+
+  arr.map {|x| rank[x]}
+end
