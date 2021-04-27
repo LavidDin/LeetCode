7477188aa4aa8b3df0d474ec561d309c97de923a
David Lin <polsara@mac.home>
2021-04-27 17:59:00 +0000

    Code challenge submission for question: 451 Sort Characters By Frequency

---
+++ 451 Sort Characters By Frequency/451sort-characters-by-frequency.rb
@@ -0,0 +1,14 @@
+# @param {String} s
+# @return {String}
+def frequency_sort(s)
+  result = ""
+  letter_freq = Hash.new { |h, k| h[k] = 0}
+
+  s.each_char { |c| letter_freq[c] += 1 }
+
+  letters = letter_freq.keys.sort_by { |k| letter_freq[k] }.reverse
+
+  letters.each { |letter| result += letter * letter_freq[letter] }
+    
+  result
+end
