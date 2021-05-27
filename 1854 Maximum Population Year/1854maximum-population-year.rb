2298463736230e4371ec9c7e305d6d727338602b
David Lin <polsara@mac.home>
2021-05-27 04:09:36 +0000

    Code challenge submission for question: 1854 Maximum Population Year

---
+++ 1854 Maximum Population Year/1854maximum-population-year.rb
@@ -0,0 +1,13 @@
+# @param {Integer[][]} logs
+# @return {Integer}
+def maximum_population(logs)
+  population_h = Hash.new(0)
+  
+  logs.each do |person|
+    (person[0]...person[1]).each do |year|
+      population_h[year] += 1
+    end
+  end
+  
+  population_h.sort_by{|year, count| [-count, year]}.first[0]
+end
