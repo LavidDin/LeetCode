fcdfc7777cdeaf543eb207d3ddd9ff671f215320
David Lin <polsara@mac.home>
2022-02-20 04:37:13 +0000

    Code challenge submission for question: 435 Non-overlapping Intervals

---
+++ 435 Non-overlapping Intervals/435non-overlapping-intervals.rb
@@ -0,0 +1,19 @@
+# @param {Integer[][]} intervals
+# @return {Integer}
+def erase_overlap_intervals(intervals)
+    intervals.sort_by!(&:last)
+    i = 1
+    count = 0
+    current = intervals[0][1]
+
+    while i < intervals.size
+        if intervals[i][0] < current
+            count += 1
+        else
+            current = intervals[i][1]
+        end
+        i += 1
+    end
+ 
+    p count
+end
