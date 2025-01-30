1234922cc6b3fcc117a95a9e09e01d81682b9e3f
polsara <david.lin71@gmail.com>
2025-01-30 23:29:42 +0000

    Code challenge submission for question: 1 Two Sum

--- 1 Two Sum/1two-sum.py
+++ 1 Two Sum/1two-sum.py
@@ -5,6 +5,5 @@ class Solution:
         for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
-                return [prevMap[diff], i]
+                return[prevMap[diff], i]
             prevMap[n] = i
-            
